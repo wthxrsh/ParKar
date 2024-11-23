3cdf0cd7f8059d645818359e77d21a9799aa05f4
Harsh <63588817+wthxrsh@users.noreply.github.com>
2024-11-23 12:40:57 +0530

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,7 +0,0 @@
-async function searchLocation(query) {
-    const apiKey = 'pk.4aa50b3509eb35021789f305ebcd4f37';
-    const response = await fetch(`https://api.locationiq.com/v1/autocomplete.php?key=${apiKey}&q=${encodeURIComponent(query)}`);
-    const data = await response.json();
-    console.log(data);
-}
-
